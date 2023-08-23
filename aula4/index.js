d8f404773e4a72abd07f5fc5979d64f0497b72b7
Larissa Sevilha <sevilha.larissa10@gmail.com>
2023-08-23 20:24:44 -0300

    biblioteca

---
+++ aula4/index.js
@@ -0,0 +1,10 @@
+class Retangulo {
+   constructor(altura,largura){
+    this.altura = altura;
+    this.largura = largura;
+   }
+}
+ 
+const retangulo = new Retangulo (20, 20)
+console.log(retangulo.altura);
+console.log(retangulo.largura);
---
+++ aula5/biblioteca.js
@@ -0,0 +1,36 @@
+class Autor{
+    constructor(nome,nacionalidade,data_de_nascimento){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.data_de_nascimento = data_de_nascimento;
+     }
+}
+class Livro{
+    constructor(titulo,ano_de_publicacao){
+        this.titulo = titulo;
+        this.ano_de_publicacao = ano_de_publicacao; 
+    }
+}
+class Usuario{
+    constructor(nome,idade){
+        this.nome = nome;
+        this.idade = idade; 
+    }
+}
+
+const autor = new Autor('larissa', 'brasileira', '10/02/2004' )
+const livro = new Livro('o menino maluquinho', '2010')
+const usuario = new Usuario('carlos', '15')
+
+//autor
+console.log(autor.nome);
+console.log(autor.nacionalidade);
+console.log(autor.data_de_nascimento);
+
+//livro
+console.log(livro.titulo);
+console.log(livro.ano_de_publicacao);
+
+//usuario
+console.log(usuario.nome);
+console.log(usuario.idade);
